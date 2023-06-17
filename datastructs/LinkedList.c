344be82029036e838a146128241bd0c21743932a
Kevin Dillon <kdillo893@gmail.com>
2023-06-17 03:53:05 -0500

    linkedlist (untested)

--- datastructs/LinkedList.c
+++ datastructs/LinkedList.c
@@ -1,5 +1,6 @@
 #include "headers/basic_ds.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
 void linkedListPrepend(linkedlist_t * theList, int newInt) {
@@ -21,37 +22,84 @@ void linkedListAppend(linkedlist_t * theList, int newInt) {
 }
 
 node_t * linkedListSearch(linkedlist_t * theList, int idx) {
-
-  return NULL;
-}
-
-int linkedListGetEntry(linkedlist_t * theList, int idx) { 
   node_t * currNode = theList->head;
 
   int currIdx = 0;
   while(currNode->next != NULL || currIdx != idx) {
-
+    //not at the idx, make current equal next, increment idx.
+    currNode = currNode->next;
+    currIdx++;
   }
 
   if (currIdx != idx) {
-    //didn't reach the index... what do?
+    //didn't reach the index... no real "didn't find" result. just give -1
 
-    return -1;
+    return NULL;
   } else {
-    return currNode->val;
+    return currNode;
   }
-
 }
 
-void linkedListSetEntry(linkedlist_t * theList, int idx, void* value) {
+int linkedListGetEntry(linkedlist_t * theList, int idx) { 
+  node_t * currNode = linkedListSearch(theList, idx);
 
+  if (currNode == NULL) {
+    //not sure what do if node not found, just give -1
+    return -1;
+  }
   
+  return currNode->val;
+}
+
+void linkedListSetEntry(linkedlist_t * theList, int idx, int value) {
+  node_t * currNode = linkedListSearch(theList, idx);
+
+  if (currNode == NULL) {
+    return;
+  }
+
+  currNode->val = value;
 }
 
 int linkedListDeleteEntry(linkedlist_t * theList, int idx) {
-  //if the delete index is outside the size, return -1 (the int returned is the index removed)
+
+  if (theList->head == NULL) {
+    return -1;
+  }
+
+  if (idx == 0) {
+    //move the head to next.
+    theList->head = theList->head->next;
+    return idx;
+  }
+
+  //search for the one before so you can connect the nodes.
+  node_t * oneBeforeNode = linkedListSearch(theList, idx - 1);
+  //if couldn't reach before OR the "node to delete" doesn't exist, just return -1 for no deleted.
+  if (oneBeforeNode == NULL || oneBeforeNode->next == NULL) {
+    return -1;
+  }
+
+  //do have a node, replace then return idx;
+  oneBeforeNode->next = oneBeforeNode->next->next;
+  return idx;
 }
 
 void linkedListDisplay(linkedlist_t * theList) {
+  
+  printf("[");
+
+  node_t * currNode = theList->head;
+  while(currNode != NULL) {
+    printf("%d", currNode->val);
+
+    //next node; if this isn't the last one (next not null), then put a comma.
+    currNode = currNode->next;
+    if (currNode != NULL) {
+      printf(", ");
+    }
+  }
+
+  printf("]\n");
 
 }
