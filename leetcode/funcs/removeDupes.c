7bc5333949a23b2c2c10373929ffc73a2d071d6d
Kevin Dillon <kdillo893@gmail.com>
2023-06-18 13:37:13 -0500

    extra problem (still no "sorted" logic)

--- leetcode/funcs/removeDupes.c
+++ leetcode/funcs/removeDupes.c
@@ -32,3 +32,37 @@ int removeDuplicates(int* nums, int numsSize){
 
   return newSize;
 }
+
+int removeDuplicatesOverTwice(int* nums, int numsSize){
+
+  //we receive pointer to array and its length...
+  //O(n^2) option
+  //traverse array; put non-dupes into the existing array,
+  //  increase result size and place non-dupes in the existing memory locations starting at 0...
+  //  to evaluate "if a dupe exists", traverse the new length.
+
+  //sorted arrays could make it O(n), because would only need to check "the other match".
+
+
+  int newSize = 0;
+
+  for (int i = 0; i < numsSize; i++) {
+    int countOfVal = 0;
+    
+    for (int j = 0; j < newSize; j++) {
+      if (nums[i] == nums[j]) {
+        countOfVal++;
+      }
+    }
+
+    //only let 2 duplicates exist.
+    if (countOfVal < 2) {
+      //not duplicate, add to the list.
+      nums[newSize] = nums[i];
+      newSize++;
+    }
+  }
+
+
+  return newSize;
+}
