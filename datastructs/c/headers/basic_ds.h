18819a2abac747863aef20ae09fd09a1af07a217
Kevin Dillon <kdillo893@gmail.com>
2024-03-28 15:52:03 -0500

    move things for ds folder

---
+++ datastructs/c/headers/basic_ds.h
@@ -0,0 +1,49 @@
+#include<stdlib.h>
+
+/** define ds structs.
+*/
+
+/** Arraylist would need "what is this an arraylist of" and "length of list", then the contents.
+  "how do I specify type on retrieve?" -> maybe just a void pointer and casting from caller.
+*/
+typedef struct ArrayList {
+  size_t elementSize; // how big is each element?
+  int length; // what data is actually here and how much? (capacity =/= length)
+  int elementCapacity; // how many elements can I hold?
+
+  //data will be "some allocation of size x length", the length can be changed.
+  //(array of void pointers).
+  void ** data;
+} arraylist_t;
+
+/** Node containing a simple int value and the next node
+*/
+typedef struct Node {
+  int val;
+  struct Node * next;
+} node_t;
+
+/** linkedlist is just "head points to node"...
+*/
+typedef struct LinkedList {
+  node_t * head;
+} linkedlist_t;
+
+/** queue is just "keep track of the front and back", new nodes go on the end.
+  "front" next refers to "next front"; this trails to back, then next front for back is null.
+  "back" added onto by "back's pointer goes to new node" for enqueue.
+  in order to "empty", remember to free the nodes before assigning null pointer to top.
+*/
+typedef struct Queue {
+  node_t * front;
+  node_t * back;
+} queue_t;
+
+/** stack is "keep track of top only", nodes come from and go to top.
+  "top" refers to the "next top";
+  pop from stack grabs node and new top pointer goes to the old top's next.
+  push onto stack is new top points to old top, then stack refers to new node top.
+*/
+typedef struct Stack {
+  node_t * top;
+} stack_t;
---
+++ datastructs/c/headers/hashtable.h
@@ -0,0 +1,48 @@
+#ifndef HASHTABLE_T
+#define HASHTABLE_T
+
+typedef struct hashtable hashtable_t;
+typedef struct bucket bucket_t;
+
+/**
+ * Linked list with key/value pair. 
+ * A bucket is start of a DS to describe key matches
+ **/
+struct bucket {
+  char *key;
+  void *val;
+  bucket_t *next;
+};
+
+/**
+ * pointer to buckets array and size
+ */
+struct hashtable {
+  unsigned long size;
+  bucket_t **buckets;
+};
+
+unsigned long hash(char *str);
+
+/** Initialize hashtable with a number of buckets. Put for a given k,v pair 
+    will place in bucket linked list with [hash() % size] index. */
+hashtable_t *make_hashtable(unsigned long size);
+
+/** Put the value in the hashtable with the given key.*/
+void  ht_put(hashtable_t *ht, char *key, void *val);
+
+/** Retrieve the value for a given key.*/
+void *ht_get(hashtable_t *ht, char *key);
+/** Delete the key/value pair in the hashtable, freeing memory.*/
+void  ht_del(hashtable_t *ht, char *key);
+/** Iterate through all buckets with information, unsorted, performing a given function (f).
+    If the function (f) returns a falsey value, break iteration. */
+void  ht_iter(hashtable_t *ht, int (*f)(char *, void *));
+/** Re-package hashtable with new amount of buckets.*/
+void  ht_rehash(hashtable_t *ht, unsigned long newsize);
+/** Free memory usage from all buckets, then free bucket array pointer and hashtable.*/
+void  free_hashtable(hashtable_t *ht);
+/** Free memory from an individual bucket (which contains a key/value pair).*/
+void  free_bucket(bucket_t *b);
+
+#endif
