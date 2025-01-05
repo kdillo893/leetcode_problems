32230227691cf13782a8f7240fe14181612d6762
Kevin Dillon <kdillo893@gmail.com>
2025-01-04 19:37:17 -0600

    tiny learning things

---
+++ codingQuestions/JS/codeSignal.js
@@ -0,0 +1,12 @@
+let arr = [
+  ['a', 'b', 'c'],
+  ['d', 'e', 'f'],
+  ['g', 'h', 'i']
+];
+
+console.log(arr[0][1]);
+//these all OoB error
+// console.log(arr[3][1]);
+// console.log(arr[0][5]);
+// console.log(arr[3]);
+console.log(arr[2]);
